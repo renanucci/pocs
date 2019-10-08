aa06b2fce4075e5207020b87d68661d119458b7d
renanucci <renanrp@gmail.com>
2019-10-08 01:34:30 -0300

    busca pela chave passada na url

--- helloworld.js
+++ helloworld.js
@@ -3,34 +3,44 @@ var ioredis = require('ioredis');
 
 http.createServer(function (req, res) 
 {
-  console.log('inicando response \n ');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('--ta rolando! \n');
-
-var ipredis = "192.168.99.100";
-
-  if(process.env.IP_REDIS_FIAP)
-  {
-    console.log("setando o ip do redis com base na variavel de ambiente")
-    ipredis = process.env.IP_REDIS_FIAP
-  }
-
   console.log("-------------------------");
   console.log("valores configurados");
   console.log("ipredis = [" + ipredis+"]");
   console.log("-------------------------");
 
+  console.log('inicando response \n ');
+  if(req.url != "/")
+  {
+    var nomechave = req.url.replace("/","");
+    console.log("nomechave = ["+nomechave+"]");
 
-  var objIoredis = new ioredis(6379, ipredis);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('--ta rolando! \n');
 
-  getRedis(objIoredis, "novotoggle").then(resultado => {
-    res.write("Resultado: ["+ JSON.stringify(resultado)+"] \n");
-  }).catch(err => {
-    res.write("deu ruim: ["+JSON.stringify(err)+"] \n");
-  }).finally(_ => {
-    res.write("--acabou \n");
+    var ipredis = "192.168.99.100";
+
+    if(process.env.IP_REDIS_FIAP)
+    {
+      console.log("setando o ip do redis com base na variavel de ambiente")
+      ipredis = process.env.IP_REDIS_FIAP
+    }
+    
+    var objIoredis = new ioredis(6379, ipredis);
+
+    getRedis(objIoredis, nomechave).then(resultado => {
+      res.write("Resultado: ["+ JSON.stringify(resultado)+"] \n");
+    }).catch(err => {
+      res.write("deu ruim: ["+JSON.stringify(err)+"] \n");
+    }).finally(_ => {
+      res.write("--acabou \n");
+      res.end();
+    })
+  }
+  else
+  {
     res.end();
-  })
+  }
+
 
   console.log('finalizando response \n');
 }).listen(8081);
